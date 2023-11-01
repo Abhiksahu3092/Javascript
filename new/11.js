de234105a2b45d712a52e6c915b518a911e88c0b
Abhik Sahu <abhiksahu2003@gmail.com>
2023-11-01 18:14:55 +0530

    selectors in js

---
+++ new/11.js
@@ -0,0 +1,10 @@
+let p_id=document.getElementById("pid");
+p_id.style.color="blue";
+
+let card_text=document.getElementsByClassName("card-text");
+card_text[0].style.color="green";
+card_text[1].style.color="red";
+card_text[2].style.color="purple";
+/*
+similarly we can use queryselector
+getquerySelector.(".card-text")*/
