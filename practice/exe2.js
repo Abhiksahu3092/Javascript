51ba845fe3c84894821e67345ac8eb75380a6ebf
Abhik Sahu <abhiksahu2003@gmail.com>
2023-11-11 11:49:13 +0530

    minor changes

--- practice/exe2.js
+++ practice/exe2.js
@@ -3,13 +3,15 @@ let n=arr.length;
 let p=true;
 while(p){
     let uip=prompt("enter your choice");
-    let cip=arr[Math.floor(Math.random()*n)]
+    let cip=arr[Math.floor(Math.random()*n)]//This function creates random answers from the system
     if(uip==cip){
         alert("tie. "+cip+" was the choice from the system");
     }
+    
     if((uip=='p' && cip=='r')||(uip=='s' && cip=='p')||(uip=='r' && cip=='s')){
         alert("you win. "+cip+" was the choice from the system");
     }
+
     if((uip=='r' && cip=='p')||(uip=='p' && cip=='s')||(uip=='s' && cip=='r')){
         alert("you lose. "+cip+" was the choice from the system");
     }
