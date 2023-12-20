91b475bbfa8c456af9bb7e644e976e4156a3fb8c
Abhik Sahu <abhiksahu2003@gmail.com>
2023-12-20 12:05:11 +0530

    BOM

--- new/9.js
+++ new/9.js
@@ -2,6 +2,8 @@ let b=document.body;
 console.log(b.firstElementChild);
 console.log(b.firstChild);
 
-const change_bg_color=()=>{
-    b.firstElementChild.style.background="green"
-};
+const change_color=()=>{
+    document.body.style.background="yellow";
+}
+
+change_color();//after defining the function we have to call the function as well for its working
