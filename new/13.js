c3249fe6c8c4b5a6dd5fe38b6f7b3ff20692e3ae
Abhik Sahu <abhiksahu2003@gmail.com>
2023-12-24 14:47:44 +0530

    will continue later

---
+++ new/13.js
@@ -0,0 +1,10 @@
+let a = document.getElementsByTagName("div")[0];
+console.log(a);
+
+//a.innerHTML = a.innerHTML + '<p>hello p</p>';
+//this is a old way of insertion
+
+let b=document.createElement('div');
+b.innerHTML='<p>This is created element</p>';
+
+a.appendChild(b);
