e5f1e7d7a8b0f6a1a306974ae8c5aeeb9338d11f
Abhik Sahu <abhiksahu2003@gmail.com>
2023-12-25 18:47:53 +0530

    Some more insertion methods

---
+++ new/14.js
@@ -0,0 +1,5 @@
+cwh.insertAdjacentHTML('beforebegin','<p>beforebegin</p>');
+cwh.insertAdjacentHTML('beforeend','<p>beforeend</p>');
+cwh.insertAdjacentHTML('afterbegin','<p>afterbegin</p>');
+cwh.insertAdjacentHTML('afterend','<p>afterend</p>');
+
