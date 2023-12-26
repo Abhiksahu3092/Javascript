e82d3a3c97878e10c639a0c163f8a749cbeda7ae
Abhik Sahu <abhiksahu2003@gmail.com>
2023-12-26 14:09:08 +0530

    practice problems on eventlisteners

---
+++ practice/exe5.js
@@ -0,0 +1,15 @@
+document.getElementById("github").addEventListener("click",function(){
+    let url="https://github.com/Abhiksahu3092";
+    window.location="https://github.com/Abhiksahu3092";
+})
+
+document.getElementById("linkedin").addEventListener("click",function(){
+    let url="https://www.linkedin.com/in/abhiksundarsahu";
+    window.location="https://www.linkedin.com/in/abhiksundarsahu";
+})
+
+
+document.getElementById("codechef").addEventListener("click",function(){
+    let url="https://www.codechef.com/users/abhiksahu2003";
+    window.location="https://www.codechef.com/users/abhiksahu2003";
+})
